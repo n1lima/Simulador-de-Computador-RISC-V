d4409bac9c3a7b68af845bb006c1dafad89ea966
n1lima <nicolima1905@gmail.com>
2025-12-04 15:37:34 -0300

    Implementando o I-type e o R-type

--- cpu/executor.py
+++ cpu/executor.py
@@ -3,6 +3,86 @@ class Executor:
         self.regs = regs
         self.memory = memory
 
-    def execute(self, decoded):
-        #futuro: executar instruções reais
-        pass
+    def execute(self, d):
+        opcode = d["opcode"]
+        funct3 = d["funct3"]
+        funct7 = d["funct7"]
+
+        if d["type"] == "I":  # I-TYPE (ADDI, ANDI, ORI, XORI)
+            if opcode == 0b0010011:  # OP-IMM
+                self.exec_op_imm(d)
+
+        elif d["type"] == "R":  # R-TYPE (ADD, SUB, AND, OR, XOR)
+            if opcode == 0b0110011:  # OP
+                self.exec_op(d)
+
+    # ----------------------------------------------
+    # I-TYPE (ADDI, ANDI, ORI, XORI)
+    # ----------------------------------------------
+    def exec_op_imm(self, d):
+        rs1_val = self.regs.read(d["rs1"])
+        imm = d["imm"]
+        funct3 = d["funct3"]
+
+        # ADDI
+        if funct3 == 0b000:
+            result = rs1_val + imm
+            self.regs.write(d["rd"], result)
+            print(f"ADDI x{d['rd']} = x{d['rs1']} + {imm} -> {result}")
+
+        # XORI
+        elif funct3 == 0b100:
+            result = rs1_val ^ imm
+            self.regs.write(d["rd"], result)
+            print(f"XORI x{d['rd']} = x{d['rs1']} XOR {imm} -> {result}")
+
+        # ORI
+        elif funct3 == 0b110:
+            result = rs1_val | imm
+            self.regs.write(d["rd"], result)
+            print(f"ORI x{d['rd']} = x{d['rs1']} OR {imm} -> {result}")
+
+        # ANDI
+        elif funct3 == 0b111:
+            result = rs1_val & imm
+            self.regs.write(d["rd"], result)
+            print(f"ANDI x{d['rd']} = x{d['rs1']} AND {imm} -> {result}")
+
+    # ----------------------------------------------
+    # R-TYPE (ADD, SUB, AND, OR, XOR)
+    # ----------------------------------------------
+    def exec_op(self, d):
+        rs1_val = self.regs.read(d["rs1"])
+        rs2_val = self.regs.read(d["rs2"])
+        funct3 = d["funct3"]
+        funct7 = d["funct7"]
+
+        # ADD
+        if funct3 == 0b000 and funct7 == 0b0000000:
+            result = rs1_val + rs2_val
+            self.regs.write(d["rd"], result)
+            print(f"ADD x{d['rd']} = x{d['rs1']} + x{d['rs2']} -> {result}")
+
+        # SUB
+        elif funct3 == 0b000 and funct7 == 0b0100000:
+            result = rs1_val - rs2_val
+            self.regs.write(d["rd"], result)
+            print(f"SUB x{d['rd']} = x{d['rs1']} - x{d['rs2']} -> {result}")
+
+        # XOR
+        elif funct3 == 0b100 and funct7 == 0b0000000:
+            result = rs1_val ^ rs2_val
+            self.regs.write(d["rd"], result)
+            print(f"XOR x{d['rd']} = x{d['rs1']} XOR x{d['rs2']} -> {result}")
+
+        # OR
+        elif funct3 == 0b110 and funct7 == 0b0000000:
+            result = rs1_val | rs2_val
+            self.regs.write(d["rd"], result)
+            print(f"OR x{d['rd']} = x{d['rs1']} OR x{d['rs2']} -> {result}")
+
+        # AND
+        elif funct3 == 0b111 and funct7 == 0b0000000:
+            result = rs1_val & rs2_val
+            self.regs.write(d["rd"], result)
+            print(f"AND x{d['rd']} = x{d['rs1']} AND x{d['rs2']} -> {result}")
--- main.py
+++ main.py
@@ -2,12 +2,16 @@ from bus.bus import Bus
 
 bus = Bus()
 
-#ADDI x1, x0, 5 no endereço 0
-bus.memory.store_word(0x0000, 0x00500093)
+bus.memory.store_word(0x0000, 0x00F00093)
 
+bus.memory.store_word(0x0004, 0x0050F113)
+
+bus.memory.store_word(0x0008, 0x0020C1B3)
+
+# Executa 3 instruções
 bus.run(3)
 
-# --> teste para parte de lógica de decodificação de instrução <--
-
-d = bus.cpu.decoder.decode(0x00500093)
-print(d)
+# Verificando resultados nos registradores
+print("x1 =", bus.cpu.regs.read(1)) 
+print("x2 =", bus.cpu.regs.read(2)) 
+print("x3 =", bus.cpu.regs.read(3)) 
